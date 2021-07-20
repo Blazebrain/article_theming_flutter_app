81b5e0455592b448007354f66be08d5c1b427202
Blazebrain <davidadegoke16@gmail.com>
2021-07-20 08:46:00 +0100

    Final wrap up of sample app for project

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Guide to Theming Flutter'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              const CustomButton('Apples'),
-              const CustomButton('Oranges'),
-              const CustomButton('Bananas'),
-            ],
+      title: 'Guide to Theming in Flutter',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.red,
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(
+            fontSize: 14.0,
+            fontFamily: 'Hind',
           ),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Guide to Theming in Flutter'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            CustomButton(),
+            CustomButton(),
+            CustomButton(),
+            const SizedBox(height: 8),
+            Container(
+              color: Theme.of(context).accentColor,
+              child: Text(
+                'Theming in Flutter',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+// ignore: use_key_in_widget_constructors
 class CustomButton extends StatelessWidget {
-  final String title;
-  // ignore: use_key_in_widget_constructors
-  const CustomButton(this.title);
-
   @override
   Widget build(BuildContext context) {
     return Container(
